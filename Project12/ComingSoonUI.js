dcd6c363c953204cb645182eb9989f7575843ea5
Ashish Dwivedi <ashishdwivedi7654321@gmail.com>
2026-03-25 23:07:12 +0530

    Update ComingSoonUI.js

--- Project12/ComingSoonUI.js
+++ Project12/ComingSoonUI.js
@@ -27,6 +27,7 @@ function updateCountdown() {
   const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  
   daysEl.textContent = String(days).padStart(2, "0");
   hoursEl.textContent = String(hours).padStart(2, "0");
   minutesEl.textContent = String(minutes).padStart(2, "0");
